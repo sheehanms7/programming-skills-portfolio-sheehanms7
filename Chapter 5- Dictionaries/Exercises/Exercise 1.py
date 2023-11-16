6d7af155c21692d4e212a990d849f271c0b61d95
sheehanms7 <sheehanms77@gmail.com>
2023-11-16 11:09:09 +0400

    Exercise 1.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise 1.py
@@ -0,0 +1,10 @@
+person_info = {'first_name': 'Sheehan',
+               'last_name': 'Mohd',
+               'age': 18,
+               'city': 'Sharjah'}
+
+
+print (f"First Name: {person_info['first_name']}")
+print (f"Last Name: {person_info['last_name']}")
+print (f"Age: {person_info['age']}")
+print (f"City: {person_info['city']}")
