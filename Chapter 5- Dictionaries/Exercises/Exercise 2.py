f1ebd0879acc173d3ee89f384744bfcf074d987d
sheehanms7 <sheehanms77@gmail.com>
2023-11-16 11:09:43 +0400

    Update Exercise 2.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise 2.py
@@ -0,0 +1,7 @@
+glossary = {'variable': 'A named storage location for holding data',
+            'function': 'A code that performs specific task',
+            'list': 'An ordered collection of items',
+            'dictionary': 'A collection of key-value pairs'}
+
+for word, meaning in glossary.items():
+    print (f"{word.capitalize()}: {meaning}\n")
