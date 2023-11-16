9c0a5c01b6fd7f313e71def453d6fbaa06d478a5
sheehanms7 <sheehanms77@gmail.com>
2023-11-16 11:06:15 +0400

    Create Exercise 5.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise 5.py
@@ -0,0 +1,10 @@
+fav_fruits = ["Mango","Orange","apple"]
+
+if "Mango" in fav_fruits:
+    print ("You really like Mango")
+
+if "Orange" in fav_fruits:
+    print ("You really like Orange")
+
+if "apple" in fav_fruits:
+    print ("You really like apple")
