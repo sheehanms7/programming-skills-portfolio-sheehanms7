be0c34bd2298e444e0af869b2c5a1dbb08c4043b
sheehanms7 <sheehanms77@gmail.com>
2023-11-16 10:41:49 +0400

    Create Exercise 5.py

---
+++ Chapter 2- Variables & Comments/Exercise 5.py
@@ -0,0 +1,5 @@
+usb_price = 10
+budget = 50
+num_usbstick = budget // usb_price
+remain = budget % usb_price
+print (f"She can buy {num_usbstick} usb sticks with {budget} Dhs, and she will have a reamaining of {remain} Dhs left ")
