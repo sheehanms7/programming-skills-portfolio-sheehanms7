d37d8d365c7ecf204071d00526d4757b28cc5d56
sheehanms7 <sheehanms77@gmail.com>
2023-11-16 11:01:11 +0400

    Create Exercise 1.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise 1.py
@@ -0,0 +1,6 @@
+alien_colour = "Blue"
+if alien_colour == "Red":
+    print ("Congratulations! You just earned 5 points for shooting the alien.")
+
+else:
+    print ("Congratulations! You just earned 10 points")
