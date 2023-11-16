a9a41b183d5d457b8121ba56fa2451ff17e070c2
sheehanms7 <sheehanms77@gmail.com>
2023-11-16 11:41:34 +0400

    Create Exercise 4.py

---
+++ Chapter 7- Functions/Exercises/Exercise 4.py
@@ -0,0 +1,6 @@
+def make_shirt(size="Large",msg="Nothing but Dubai"):
+    print (f"Made a shirt of size {size} with the message:'{msg}'")
+make_shirt()
+
+make_shirt (size="Large")
+make_shirt (size="Large", msg="I love Dubai")
