b920f5b281d7b15e251e6f76542bc45ca212a0d3
sheehanms7 <sheehanms77@gmail.com>
2023-11-16 11:03:41 +0400

    Create Exercise 3.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise 3.py
@@ -0,0 +1,12 @@
+alien_colour = "yellow"
+if alien_colour == "greean":
+    print ("Congratulations! You just earned 5 points")
+
+elif alien_colour == "yellow":
+    print ("Congratulations! You just earned 10 points")
+
+elif alien_colour == "red":
+    print ("Congratulations! You just earned 15 points")
+
+else :
+    print ("Invalid colour")
