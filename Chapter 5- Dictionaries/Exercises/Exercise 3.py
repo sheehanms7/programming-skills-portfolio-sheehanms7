3763b3201c1346facf3b02e49f3d6e8acb3c004d
sheehanms7 <sheehanms77@gmail.com>
2023-11-16 11:10:01 +0400

    Update Exercise 3.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise 3.py
@@ -0,0 +1,10 @@
+glossary = {'variable': 'A named storage location for holding data',
+            'function': 'A code that performs specific task',
+            'list': 'An ordered collection of items',
+            'dictionary': 'A collection of key-value pairs',
+            'Tuple': 'An ordered, immutable sequence of elements',
+            'Boolean':'A data that express values as True or False',
+            'String': 'Statements surrounded with one or more quotations'}
+
+for word, meaning in glossary.items():
+    print (f"{word}:\n{meaning}\n")
