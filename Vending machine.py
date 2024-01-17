ad5d3cce4818d5323598ef3abda46de5dd8edbb5
sheehanms7 <144213636+sheehanms7@users.noreply.github.com>
2024-01-17 11:43:13 +0400

    Create Vending machine.py

---
+++ Vending machine.py
@@ -0,0 +1,248 @@
+print ('''░█░█░█▀▀░█░░░█▀▀░█▀█░█▄█░█▀▀░░░▀█▀░█▀█░░░█▀▀░█░█░█▀▀░█▀▀░█░█░█▀█░█▀█░▀░█▀▀
+░█▄█░█▀▀░█░░░█░░░█░█░█░█░█▀▀░░░░█░░█░█░░░▀▀█░█▀█░█▀▀░█▀▀░█▀█░█▀█░█░█░░░▀▀█
+░▀░▀░▀▀▀░▀▀▀░▀▀▀░▀▀▀░▀░▀░▀▀▀░░░░▀░░▀▀▀░░░▀▀▀░▀░▀░▀▀▀░▀▀▀░▀░▀░▀░▀░▀░▀░░░▀▀▀
+░█░█░█▀▀░█▀█░█▀▄░▀█▀░█▀█░█▀▀░░░█▄█░█▀█░█▀▀░█░█░▀█▀░█▀█░█▀▀                
+░▀▄▀░█▀▀░█░█░█░█░░█░░█░█░█░█░░░█░█░█▀█░█░░░█▀█░░█░░█░█░█▀▀                
+░░▀░░▀▀▀░▀░▀░▀▀░░▀▀▀░▀░▀░▀▀▀░░░▀░▀░▀░▀░▀▀▀░▀░▀░▀▀▀░▀░▀░▀▀▀                ''')
+
+
+99
+
+
+items = {
+    'item1': {
+        'category': 'Energy Drinks',
+        'name': 'Red Bull',
+        'code': 1,
+        'price': 9.00,
+        'stock': 14
+    },
+    'item2': {
+        'category': 'Energy Drinks',
+        'name': 'Power Horse',
+        'code': 2,
+        'price': 8.50,
+        'stock': 15
+    },
+    'item3': {
+        'category': 'Energy Drinks',
+        'name': 'Monster',
+        'code': 3,
+        'price': 11.00,
+        'stock': 15
+    },
+    'item4': { 
+        'category': 'Energy Drinks',
+        'name': 'Pokari Sweat',
+        'code': 4,
+        'price': 8.50,
+        'stock': 13
+    },
+    'item5': {
+        'category': 'Cold Drinks',
+        'name': 'Mountain Dew',
+        'code': 5,
+        'price': 2.50,
+        'stock': 15
+    },
+    'item6': {
+        'category': 'Cold Drinks',
+        'name': 'Fanta',
+        'code': 6,
+        'price': 2.50,
+        'stock': 15
+    },
+    'item7': {
+        'category': 'Cold Drinks',
+        'name': 'Pepsi',
+        'code': 7,
+        'price': 2.50,
+        'stock': 15
+    },
+    'item8': {
+        'category': 'Cold Drinks',
+        'name': 'Sprite',
+        'code': 8,
+        'price': 2.50,
+        'stock': 15
+    },
+    'item9': {
+        'category': 'Cold Drinks',
+        'name': 'Orange Juice',
+        'code': 9,
+        'price': 3.50,
+        'stock': 15
+    },
+    'item10': {
+        'category': 'Snacks',
+        'name': 'Cheetos Flamin Hot',
+        'code': 10,
+        'price': 3.50,
+        'stock': 15
+    },
+    'item11': {
+        'category': 'Snacks',
+        'name': 'Oman Chips',
+        'code': 11,
+        'price': 2.50,
+        'stock': 15
+    },
+    'item12': {
+        'category': 'Snacks',
+        'name': 'Sandos Wafer',
+        'code': 12,
+        'price': 3.50,
+        'stock': 15
+    },
+    'item13': {
+        'category': 'Chocolates',
+        'name': 'Kit Kat',
+        'code': 13,
+        'price': 2.00,
+        'stock': 15
+    },
+    'item14': {
+        'category': 'Chocolates',
+        'name': 'Skittles',
+        'code': 14,
+        'price': 4.50,
+        'stock': 15
+    },
+    'item15': {
+        'category': 'Chocolates',
+        'name': 'Galaxy Smooth Milk',
+        'code': 15,
+        'price': 4.50,
+        'stock': 15
+    },
+    'item16': {
+        'category': 'Chocolates',
+        'name': 'Twix',
+        'code': 16,
+        'price': 4.50,
+        'stock': 15
+    },
+}
+
+
+print("Category:", items['item1']['category'])
+print("Name:", items['item1']['name'])
+print("Code:", items['item1']['code'])
+print("Price:", items['item1']['price'])
+print("Stock:", items['item1']['stock'])
+
+print("Category:", items['item2']['category'])
+print("Name:", items['item2']['name'])
+print("Code:", items['item2']['code'])
+print("Price:", items['item2']['price'])
+print("Stock:", items['item2']['stock']) 
+
+print("Category:", items['item3']['category'])
+print("Name:", items['item3']['name'])
+print("Code:", items['item3']['code'])
+print("Price:", items['item3']['price'])
+print("Stock:", items['item3']['stock'])
+
+print("Category:", items['item4']['category'])
+print("Name:", items['item4']['name'])
+print("Code:", items['item4']['code'])
+print("Price:", items['item4']['price'])
+print("Stock:", items['item4']['stock'])
+
+print("Category:", items['item5']['category'])
+print("Name:", items['item5']['name'])
+print("Code:", items['item5']['code'])
+print("Price:", items['item5']['price'])
+print("Stock:", items['item5']['stock'])
+
+print("Category:", items['item6']['category'])
+print("Name:", items['item6']['name'])
+print("Code:", items['item6']['code'])
+print("Price:", items['item6']['price'])
+print("Stock:", items['item6']['stock'])
+
+print("Category:", items['item7']['category'])
+print("Name:", items['item7']['name'])
+print("Code:", items['item7']['code'])
+print("Price:", items['item7']['price'])
+print("Stock:", items['item7']['stock'])
+
+print("Category:", items['item8']['category'])
+print("Name:", items['item8']['name'])
+print("Code:", items['item8']['code'])
+print("Price:", items['item8']['price'])
+print("Stock:", items['item8']['stock'])
+
+print("Category:", items['item9']['category'])
+print("Name:", items['item9']['name'])
+print("Code:", items['item9']['code'])
+print("Price:", items['item9']['price'])
+print("Stock:", items['item9']['stock'])
+
+print("Category:", items['item10']['category'])
+print("Name:", items['item10']['name'])
+print("Code:", items['item10']['code'])
+print("Price:", items['item10']['price'])
+print("Stock:", items['item10']['stock'])
+
+print("Category:", items['item11']['category'])
+print("Name:", items['item11']['name'])
+print("Code:", items['item11']['code'])
+print("Price:", items['item11']['price'])
+print("Stock:", items['item11']['stock'])
+
+print("Category:", items['item12']['category'])
+print("Name:", items['item12']['name'])
+print("Code:", items['item12']['code'])
+print("Price:", items['item12']['price'])
+print("Stock:", items['item12']['stock'])
+
+print("Category:", items['item13']['category'])
+print("Name:", items['item13']['name'])
+print("Code:", items['item13']['code'])
+print("Price:", items['item13']['price'])
+print("Stock:", items['item13']['stock'])
+
+print("Category:", items['item14']['category'])
+print("Name:", items['item14']['name'])
+print("Code:", items['item14']['code'])
+print("Price:", items['item14']['price'])
+print("Stock:", items['item14']['stock'])
+
+print("Category:", items['item15']['category'])
+print("Name:", items['item15']['name'])
+print("Code:", items['item15']['code'])
+print("Price:", items['item15']['price'])
+print("Stock:", items['item15']['stock'])
+
+print("Category:", items['item16']['category'])
+print("Name:", items['item16']['name'])
+print("Code:", items['item16']['code'])
+print("Price:", items['item16']['price'])
+print("Stock:", items['item16']['stock'])
+
+print("Please select an item:")
+for key, item in items.items():
+    print(f"{key}. {item['name']} - Dhs {item['price']}")
+
+choice = input("Enter the item no. you wish for...")
+
+if choice in items:
+    choice_item = items [choice]
+    print ("Your item is ",{choice_item['name']})
+    amount = choice_item['price']
+
+    while amount > 0:
+        try:
+            payment = float(input(f"Please insert Dhs {amount:.2f}: "))
+            if payment >= amount:
+                balance = payment - amount
+                print(f"Thank you for purchasing! Your balance is ${balance:.2f}.")
+                break
+            else:
+                print("Insufficient payment. Please insert more money.")
+                amount -= payment
+        except ValueError:
+            print("Invalid payment amount. Please enter a valid number.")
+else:
+    print("Invalid selection. Please try again.")
